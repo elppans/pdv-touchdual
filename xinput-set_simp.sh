db9e4c3c4acffee898bb9cd6a5f13085191a4263
elppans <elppansmk@gmail.com>
2026-03-05 16:52:03 -0300

    Update xinput-set_simp.sh

--- xinput-set_simp.sh
+++ xinput-set_simp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+TEMPO='45'
 MONITOR=($(xrandr | grep " connected" | head -n1 | awk '{print $1}'))
 TOUCH_ID=($(xinput list | grep -iv "Mouse" | grep -i "ILITEK ILITEK-TP" | grep -o "id=[0-9]*" | cut -d "=" -f 2))
 export MONITOR
@@ -9,5 +10,11 @@ echo "TOUCH_ID=$TOUCH_ID" | tee -a /tmp/xinput-set
 echo
 echo -e "xinput --map-to-output "${TOUCH_ID}" "${MONITOR}"\n" | tee -a /tmp/xinput-set
 
-for i in `seq 15 -1 1` ; do echo -ne "$i Seg.\r" ; sleep 1 ; done
+if [ -f /Zanthus/Zeus/pdvJava/sleep-gui ]; then
+	chmod +x /Zanthus/Zeus/pdvJava/sleep-gui
+	/Zanthus/Zeus/pdvJava/sleep-gui "$TEMPO"
+else
+	for i in `seq "$TEMPO" -1 1` ; do echo -ne "$i Seg.\r" ; sleep 1 ; done
+fi
+
 xinput --map-to-output "${TOUCH_ID}" "${MONITOR}"
