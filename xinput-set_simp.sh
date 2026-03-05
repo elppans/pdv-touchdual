47e8514d48e65803b1b08fdec328773469eb5230
elppans <elppansmk@gmail.com>
2026-03-05 17:03:16 -0300

    Update xinput-set_simp.sh

--- xinput-set_simp.sh
+++ xinput-set_simp.sh
@@ -10,11 +10,17 @@ echo "TOUCH_ID=$TOUCH_ID" | tee -a /tmp/xinput-set
 echo
 echo -e "xinput --map-to-output "${TOUCH_ID}" "${MONITOR}"\n" | tee -a /tmp/xinput-set
 
+TEMPO=${TEMPO:-15}
 if [ -f /Zanthus/Zeus/pdvJava/sleep-gui ]; then
-	chmod +x /Zanthus/Zeus/pdvJava/sleep-gui
-	/Zanthus/Zeus/pdvJava/sleep-gui "$TEMPO"
+    chmod +x /Zanthus/Zeus/pdvJava/sleep-gui
+    /Zanthus/Zeus/pdvJava/sleep-gui "$TEMPO"
 else
-	for i in `seq "$TEMPO" -1 1` ; do echo -ne "$i Seg.\r" ; sleep 1 ; done
+    # O seq pode falhar se $TEMPO for algo estranho
+    for i in $(seq "$TEMPO" -1 1) ; do 
+        echo -ne "Aguardando: $i Segundos...\r"
+        sleep 1
+    done
+    echo -e "\nMapeando touch..." # Limpa a linha do \r
 fi
 
 xinput --map-to-output "${TOUCH_ID}" "${MONITOR}"
